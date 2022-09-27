377b980210c8676336b6a3139a58deb83c045b10
yebityon <thebigmercury@gmail.com>
2022-09-27 14:12:21 +0900

    update comment

--- Data/java8/src/omni_binary/build.sh
+++ Data/java8/src/omni_binary/build.sh
@@ -15,5 +15,6 @@ javac -g Main.java
 echo run selogger with omni
 java  -javaagent:../../../selogger/target/selogger-0.5.0.jar=output=selogger_out,format=omnibinary Main
 
+# Please Make sure execute following command at ./origin
 echo run selogger.reader
 java -classpath ../../../selogger/target/selogger-0.5.0.jar selogger.reader.LogPrinter selogger_out
