d53ad944c112efe842114ad24f8a30829418d328
yamaoka <yamaoka.atsuhito@gmail.com>
2023-09-11 15:57:36 +0900

    i-2のmオプション用に変更

--- getdiff.bash
+++ getdiff.bash
@@ -1,19 +1,18 @@
-id=$1
-
-# origin=Data/log/i-${id}o
-# target=Data/log/i-${id}n
-origin=/data/selog-2023-yamaoka/log/i-${id}o
-target=/data/selog-2023-yamaoka/log/i-${id}n
+# id=$1
 
 hashes=(
     flow
     param
 )
-echo origin=${origin}
-echo target=${target}
 
-for hash in "${hashes[@]}"; do
-    ./POLDAM -o ${origin} -t ${target} -d ${id}_${hash} --$hash
+method=-m failOnUnknown
+
+for id in $(seq 2 2);do
+    for hash in "${hashes[@]}"; do
+        origin=/data/selog-2023-yamaoka/log/i-${id}o
+        target=/data/selog-2023-yamaoka/log/i-${id}n
+        build/POLDAM -o ${origin} -t ${target} -d /data/selog-2023-yamaokapoldam_m/${id}_${hash}_m -m failOnUnknown --$hash
+    done
 done
 
 echo origin=${origin}
