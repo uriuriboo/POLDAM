a4a57e56cac218952a0507a73cd57832bb7c4bf0
yamaoka <yamaoka.atsuhito@gmail.com>
2023-10-01 09:50:54 +0900

    オプション変更

--- get_diff_m.bash
+++ get_diff_m.bash
@@ -4,12 +4,13 @@ hashes=(
     param
 )
 id=2
-method="-m failOnUnknown"
+method="failOnUnknown"
+class="MoshiConverterFactoryTest"
 
 for hash in "${hashes[@]}"; do
     origin=/data/selog-2023-yamaoka/log/i-${id}o
     target=/data/selog-2023-yamaoka/log/i-${id}n
-    build/POLDAM -o ${origin} -t ${target} -d /data/selog-2023-yamaoka/poldam_m/${id}_${hash}_m $method --$hash
+    build/POLDAM -o ${origin} -t ${target} -d /data/selog-2023-yamaoka/poldam_m/${id}_${hash}_m_c -c $class -m $method --$hash
 done
 
 echo origin=${origin}
