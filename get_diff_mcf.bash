54bee2e06f93d0f6c5410f0a92a77442570742ef
yamaoka <yamaoka.atsuhito@gmail.com>
2023-10-30 13:56:53 +0900

    使用するシェルスクリプトを追加

---
+++ get_diff_mcf.bash
@@ -0,0 +1,22 @@
+
+hashes=(
+    flow
+    param
+)
+id=2
+method="failOnUnknown"
+class="retrofit2/converter/moshi/MoshiConverterFactoryTest"
+flib="nextName"
+# id=7
+# class="com/alibaba/druid/bvt/filter/log/Slf4jLogFilterTest"
+# method="test_slf4j"
+# flib="bind"
+
+for hash in "${hashes[@]}"; do
+    origin=/data/selog-2023-yamaoka/log/i-${id}o
+    target=/data/selog-2023-yamaoka/log/i-${id}n
+    build/POLDAM -o ${origin} -t ${target} -d /data/selog-2023-yamaoka/poldam_tmp/${id}_${hash}_m_c -c $class -m $method --$hash -f $flib 
+done
+
+echo origin=${origin}
+echo target=${target}
